50448449e11e095a0540aa2b57d9f7ef51c229f9
jay659 <jp500861@gmail.com>
2024-02-08 13:53:44 -0700

    Updates

---
+++ precache-manifest.1982f9babbbe7f9ca02efde68708d990.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5a94f0985dbded1ce759bc86db8ea6",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "d74379aa12d56226fa32",
+    "url": "/my-portfolio/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "811b2a00a329c4252777",
+    "url": "/my-portfolio/static/css/main.0d785890.chunk.css"
+  },
+  {
+    "revision": "d74379aa12d56226fa32",
+    "url": "/my-portfolio/static/js/2.22cdf20a.chunk.js"
+  },
+  {
+    "revision": "f3b3211170c61884afc00106189a2238",
+    "url": "/my-portfolio/static/js/2.22cdf20a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811b2a00a329c4252777",
+    "url": "/my-portfolio/static/js/main.90656456.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/my-portfolio/static/js/main.90656456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "20e45d9afe66e0e6533dcd44fb9a7846",
+    "url": "/my-portfolio/static/media/dev.20e45d9a.svg"
+  },
+  {
+    "revision": "f6dba401e017c2821e27a21bc62808bb",
+    "url": "/my-portfolio/static/media/email-2.f6dba401.svg"
+  },
+  {
+    "revision": "930a6325796ee92cb1356638c425541c",
+    "url": "/my-portfolio/static/media/github.930a6325.svg"
+  },
+  {
+    "revision": "0f26720bbcd76cf8df11187be7dc0a44",
+    "url": "/my-portfolio/static/media/gmail.0f26720b.svg"
+  },
+  {
+    "revision": "8b6c95d6af25024abd40bb045f16d307",
+    "url": "/my-portfolio/static/media/instagram.8b6c95d6.svg"
+  },
+  {
+    "revision": "03c7255a5f023100a65e54b64d39e40c",
+    "url": "/my-portfolio/static/media/linkedin.03c7255a.svg"
+  },
+  {
+    "revision": "3babbd64c47cc7afea454a41e97a12df",
+    "url": "/my-portfolio/static/media/location.3babbd64.svg"
+  }
+]);
